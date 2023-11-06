11dfb76fdf7d01330c5e849e91e00654120951c8
Admin <Admin@DESKTOP-JBS9UAA.lan>
2023-11-06 06:11:38 +0530

    I am uploading my whole  project

---
+++ src/myapp_prac/Welcome_test.java
@@ -0,0 +1,13 @@
+package myapp_prac;
+
+public class Welcome_test {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("This is my first java line");
+		System.out.println("syso ctrl+space is the shortcut for the print statement");
+
+	}
+
+}
