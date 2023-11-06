fef8abe5330e55ce284d91c112dbae1138f2e461
Admin <Admin@DESKTOP-JBS9UAA.lan>
2023-11-06 06:22:11 +0530

    1st attempt

--- src/myapp_prac/Welcome_test.java
+++ src/myapp_prac/Welcome_test.java
@@ -7,7 +7,7 @@ public class Welcome_test {
 		
 		System.out.println("This is my first java line");
 		System.out.println("syso ctrl+space is the shortcut for the print statement");
-
+		System.out.println("First Try");
 	}
 
 }
